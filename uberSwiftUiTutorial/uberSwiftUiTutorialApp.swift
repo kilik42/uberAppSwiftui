1c930f9ff2db43f88a64ae2c1c576457609ffba8
marvin evins <marvinevins@yahoo.com>
2022-11-03 20:06:43 -0500

    Initial Commit

---
+++ uberSwiftUiTutorial/uberSwiftUiTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  uberSwiftUiTutorialApp.swift
+//  uberSwiftUiTutorial
+//
+//  Created by marvin evins on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct uberSwiftUiTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
